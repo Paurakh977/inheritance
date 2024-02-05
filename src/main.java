e2ec633b7b018c10d0a604afd89e9057e5672b8e
“Paurakh977” <pandeypaurakhraj@gmail.com>
2024-02-05 12:23:11 +0545

    done

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import model.Member;
+import model.Staff;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c1=new Circle(5);
+        c1.getArea();
+
+        Member m1 = new Member("Paurakh",18,12000,9841298);
+        Staff s1 = new Staff("Paurakh",18,19990,9829323, 2);
+    }
+
+}
